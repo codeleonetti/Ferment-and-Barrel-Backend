9fe89c8b0f5793b20bba01b9e1874231ed843f95
john <john.d.leonetti@gmail.com>
2021-07-01 20:14:24 -0700

    added destroy for beers

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -3,14 +3,14 @@ class BreweriesController < ApplicationController
 
   # GET /breweries
   def index
-    if params[:brewery_id]
-      @brewery = Brewery.find(params[:brewery_id])
-      @breweries = @brewery.beers
-    #grab beer ids for nested route
-    else
+    # if params[:brewery_id]
+    #   @brewery = Brewery.find(params[:brewery_id])
+    #   @breweries = @brewery.beers
+    # #grab beer ids for nested route
+    # else
       @breweries = Brewery.all
   
-    end
+    #end
     render json: @breweries
   end
 
